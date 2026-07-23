036477f5c0977a937e59d9fda2f6842eadf990ce
Abdur-Rehman15 <abdur.rehman8872@gmail.com>
2026-07-23 19:39:23 +0500

    logout corrected

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -86,7 +86,7 @@ export const Navbar = ({ role }: NavbarProps) => {
 
   const handleLogout = () => {
     clearSession();
-    navigate('/login');
+    navigate('/');
   };
 
   const handleOpenEditModal = () => {
